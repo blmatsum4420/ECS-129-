8a40c90d9065c2bea54da218c712781211e43540
Brandon Matsumoto <mahtzaman123@gmail.com>
2022-02-09 12:51:04 -0800

    added random fxn, added overlap fxn, need to fix max min fxn to take into account radisu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import math 
@@ -30,41 +30,62 @@ def main():
 
 def max_min():
     #do we need to take into account radius?  I assume so 
-    x_coords = []
-    y_coords = []
-    z_coords = []
+    x_coords_plusrad = []
+    y_coords_plusrad = []
+    z_coords_plusrad = []
+    x_coords_minusrad = []
+    y_coords_minusrad = []
+    z_coords_minusrad = []
     for obj in sphere_storage:
-        x_coords.append(obj.x)
-        y_coords.append(obj.y)
-        z_coords.append(obj.z)
-    xmax = max(x_coords)
-    xmin = min(x_coords)
-    ymax = max(y_coords)
-    ymin = min(y_coords)
-    zmax = max(z_coords)
-    zmin = min(z_coords)'
-    return 
+        x_coords_plusrad.append(float(obj.x)+float(obj.radius))
+        y_coords_plusrad.append(float(obj.y)+float(obj.radius))
+        z_coords_plusrad.append(float(obj.z)+float(obj.radius))
+        x_coords_minusrad.append(float(obj.x)-float(obj.radius))
+        y_coords_minusrad.append(float(obj.y)-float(obj.radius))
+        z_coords_minusrad.append(float(obj.z)-float(obj.radius))
+    xmax = math.ceil(max(x_coords_minusrad))
+    xmin = math.floor(min(x_coords_minusrad))
+    ymax = math.ceil(max(y_coords_minusrad))
+    ymin = math.floor(min(y_coords_minusrad))
+    zmax = math.ceil(max(z_coords_minusrad))
+    zmin = math.floor(min(z_coords_minusrad))
+    return xmax,xmin,ymax,ymin,zmax,zmin
+
+def random_points():
+    n_input = int(input("Please give N number of points: "))
+    for i in range(n_input):
+        random_plots.append((random.randrange(xmin,xmax),random.randrange(ymin,ymax)))
+
+def overlap_check():
+    overlap_in_circle = 0
+    no_overlap_in_circle = 0
+    for out in random_plots:
+        for center in sphere_storage:
+            if ((float(out[0]) - float(center.x))**2) + ((float(out[1]) - float(center.y))**2) < float(center.radius)**2:
+                overlap_in_circle += 1
+            else:
+                no_overlap_in_circle += 1
+    return overlap_in_circle, no_overlap_in_circle
+
+def protein_vol():
+#quation is (xmax-xmin)(ymax-ymin)(zmax-zmin)(points in union of balls/total of point)
 
 def plot_data():
     for sphere in sphere_storage:
-        plt.scatter(float(sphere.x),float(sphere.y))
+        plt.scatter(float(sphere.x),float(sphere.y), c = 'blue')
     for points in random_plots:
-        plt.scatter(float(points[0]),float(points[1]))
+        plt.scatter(float(points[0]),float(points[1]), c = 'red')
     plt.xlabel('x-axis')
     plt.ylabel('y-axis')
     plt.title('sample_data')
     plt.show()
 
-def random_points():
-    n_input = int(input("Please give N number of points"))
-    for i in range(n_input):
-        random_plots.append((random.randrnage(xmin,xmax),random.randrnange(ymin,ymax)))
-
-
-
 #will auto run main fxn at start of program being run
 if __name__ == "__main__":
-    main()
-    max_min()   
+    main()   
+    #trash code practices here
+    xmax,xmin,ymax,ymin,zmax,zmin = max_min()
     random_points()
+    overlap,noverlap = overlap_check()
+    print(overlap,noverlap)
     plot_data()
