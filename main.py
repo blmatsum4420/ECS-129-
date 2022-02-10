74bd0a92b1032660f25a4b55bb86d96bdd022ab9
Brandon Matsumoto <mahtzaman123@gmail.com>
2022-02-09 22:17:23 -0800

    Still getting error and unsure why

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import math
 
 sphere_storage = []
 random_plots = []
+x_list = []
+y_list = []
+z_list = []
 
 class Point (object):
     def __init__(self, x, y, z, radius):
@@ -20,6 +23,12 @@ def main():
         cur_coords = line
         # split each line by whitespace and get the 3-D coordinates and radius
         x,y,z,rad = cur_coords.split()
+        x_list.append(float(x)+float(rad))
+        y_list.append(float(y)+float(rad))
+        z_list.append(float(z)+float(rad))
+        x_list.append(float(x)-float(rad))
+        y_list.append(float(y)-float(rad))
+        z_list.append(float(z)-float(rad))
         # store each point in the object
         cur_point = Point(x,y,z,rad)
         # add object to an array of points
@@ -29,32 +38,20 @@ def main():
     return
 
 def max_min():
-    #do we need to take into account radius?  I assume so 
-    #issue as proper max and min not coming out
-    x_coords = []
-    y_coords = []
-    z_coords = []
-    for obj in sphere_storage:
-        x_coords.append(float(obj.x)+float(obj.radius))
-        y_coords.append(float(obj.y)+float(obj.radius))
-        z_coords.append(float(obj.z)+float(obj.radius))
-        x_coords.append(float(obj.x)-float(obj.radius))
-        y_coords.append(float(obj.y)-float(obj.radius))
-        z_coords.append(float(obj.z)-float(obj.radius))
-    xmax = math.ceil(max(x_coords))
-    xmin = math.floor(min(x_coords))
-    ymax = math.ceil(max(y_coords))
-    ymin = math.floor(min(y_coords))
-    zmax = math.ceil(max(z_coords))
-    zmin = math.floor(min(z_coords))
+    xmax = max(x_list)
+    xmin = min(x_list)
+    ymax = max(y_list)
+    ymin = min(y_list)
+    zmax = max(z_list)
+    zmin = min(z_list)
     return xmax,xmin,ymax,ymin,zmax,zmin
 
-def random_points():
+def random_points(xmax,xmin,ymax,ymin):
     n_input = int(input("Please give N number of points: "))
     for i in range(n_input):
         #is it good to add to a outside list from a fxn for storage?
-        #how can we append to a global list but cannot use global varaibles without declaring
-        random_plots.append((random.randrange(xmin,xmax),random.randrange(ymin,ymax)))
+        random_plots.append((random.randrange(int(math.floor(xmin)),int(math.ceil(xmax))),random.randrange(int(math.floor(ymin)),int(math.ceil(ymax)))))
+        # random_plots.append(random.randrange(int(math.floor(xmin))),int(math.ceil(xmax)))),random.randrange(int((math.floor(ymin)),int((math.ceil(ymax))))))
     return n_input
 
 def overlap_check():
@@ -62,26 +59,28 @@ def overlap_check():
     union_of_circle = 0
     for out in random_plots:
         for center in sphere_storage:
-            if ((float(out[0]) - float(center.x))**2) + ((float(out[1]) - float(center.y))**2) < float(center.radius)**2:
+            if (((float(out[0]) - float(center.x))**2) + ((float(out[1]) - float(center.y))**2)) <= float(center.radius)**2:
                 union_of_circle += 1
                 break
     return union_of_circle
 
-def protein_vol(xmax,xmin,ymax,ymin,zmax,zmin,n_input,union_of_circle):
+def protein_vol(xmax,xmin,ymax,ymin,zmax,zmin,union_of_circle,n_input):
 #equation is (xmax-xmin)(ymax-ymin)(zmax-zmin)(points in union of balls/total of point)
 #easier way to turn everything into a float at once?
+    print("check if legit",xmax,xmin,ymax,ymin,zmax,zmin,union_of_circle,n_input)
     volume_of_protein = (xmax-xmin)*(ymax-ymin)*(zmax-zmin)*(union_of_circle/n_input)
     return volume_of_protein
 
-def plot_data():
-    for sphere in sphere_storage:
-        plt.scatter(float(sphere.x),float(sphere.y), c = 'blue')
-    for points in random_plots:
-        plt.scatter(float(points[0]),float(points[1]), c = 'red')
-    plt.xlabel('x-axis')
-    plt.ylabel('y-axis')
-    plt.title('sample_data')
-    plt.show()
+# def plot_data():
+## will plot and display all data points
+#     for sphere in sphere_storage:
+#         plt.scatter(float(sphere.x),float(sphere.y), c = 'blue')
+#     for points in random_plots:
+#         plt.scatter(float(points[0]),float(points[1]), c = 'red')
+#     plt.xlabel('x-axis')
+#     plt.ylabel('y-axis')
+#     plt.title('sample_data')
+#     plt.show()
 
 #will auto run main fxn at start of program being run
 if __name__ == "__main__":
@@ -91,12 +90,13 @@ if __name__ == "__main__":
     xmax,xmin,ymax,ymin,zmax,zmin = max_min()
     print("max/min",xmax,xmin,ymax,ymin,zmax,zmin)
     #request from user number of random points wanted
-    n_input = random_points()
+    n_input = random_points(xmax,xmin,ymax,ymin)
     # checks for all points that are in union of a circle
+    # test_max_min()
     union_of_circle = overlap_check()
     print("# of points in union of circle: ", union_of_circle)
     #calculates volume of our protein
-    volume_of_protein = protein_vol(xmax,xmin,ymax,ymin,zmax,zmin,n_input,union_of_circle)
+    volume_of_protein = protein_vol(xmax,xmin,ymax,ymin,zmax,zmin,union_of_circle,n_input)
     #plots all data by center points only
     print("volume of protein: ",volume_of_protein)
-    plot_data()
+    # plot_data()
